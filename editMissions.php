5999f0fd9ac559f55827eeddccd34fc37ecb79f2
joao pedro <joaopedrolimar@gmail.com>
2024-04-19 00:03:32 -0300

    tetttt

--- editMissions.php
+++ editMissions.php
@@ -1,10 +1,33 @@
 <?php
 
+if(isset($_POST['submit'])){
+
 include_once "conexao.php";
 
-$sql = "SELECT * FROM events ORDER BY id DESC";
+$id = $_GET['id'];
 
-$result = $conexao-> query($sql);
+$sqlSelect = "SELECT * FROM events WHERE id=$id";
+
+$result = $conexao->query($sqlSelect);
+
+if($result->num_rows > 0){
+
+
+    while($user_data = mysqli_fetch_assoc($result)){
+
+        $title = $user_data['title'];
+        $color =  $user_data['color'];
+        $start =  $user_data['start'];
+        $end =  $user_data['end'];
+
+        print_r($title);
+    }
+
+    }else{
+       echo 'erro';
+    }
+
+}
 
 ?>
 
@@ -59,17 +82,13 @@ $result = $conexao-> query($sql);
             cursor: pointer;
         }
 
-        .table-bg{
-            background:rgb(0, 0, 0,0.3);
-            border-radius: 15px 15px 0px 0px;
-        }
 
     </style>
 
 </head>
 
 <body>
-    <a href="/test/index.php">voltar pro calendario </a>
+    <a href="/test/form.php">volta </a>
 
     <h2>Criar Nova Missão</h2>
     
@@ -106,47 +125,6 @@ $result = $conexao-> query($sql);
     </div>
 
 
-    <div class="m-5 table-bg ">
-            <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">id</th>
-                <th scope="col">title</th>
-                <th scope="col">color</th>
-                <th scope="col">start</th>
-                <th scope="col">end</th>
-                <th scope="col">...</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                while($user_data = mysqli_fetch_assoc($result))
-                {
-                    echo "<tr>";
-                    echo "<td>".$user_data['id']."</td>";
-                    
-                    echo "<td>".$user_data['title']."</td>";
-                    
-                    echo "<td>".$user_data['color']."</td>";
-                    echo "<td>".$user_data['start']."</td>";
-                    echo "<td>".$user_data['end']."</td>";
-                    echo 
-                    "<td>
-                        <a class = 'btn btn-primary' href = 'editMissions.php?id=$user_data[id]'>
-                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-fill' viewBox='0 0 16 16'>
-                            <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z'/>
-                        </svg>
-                        </a>
-                    </td>";
-                    echo "<tr>";
-                }
-
-
-            ?>
-           
-        </tbody>
-        </table>
-    </div>
 
 
     <script>
